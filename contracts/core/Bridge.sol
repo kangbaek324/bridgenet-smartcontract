18302b9a32b54978a3cb4e952f87156bc49d522a
kangbaekho <kbh081004@gmail.com>
2025-11-06 01:35:14 +0900

    feat: 교환요청, 취소, 승인 기능 완성
    refactor: 체인 배열 리스트 삭제 및 리펙토링

--- contracts/core/Bridge.sol
+++ contracts/core/Bridge.sol
@@ -4,18 +4,14 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Bridge is Ownable {
-
-    // error
-    error WhiteListUnauthorizedAccount(address _address);
-    error IncorrectChainId(uint256 chainId);
-    error ChainAlreadyExists(uint256 chainId);
-    error ChainAlreadyRemoved(uint256 chainId);
+    uint256 requestIdCount;
 
     // enum
     enum RequestStatus {
         pending,
         approved,
-        rejected
+        rejected,
+        canceled
     }
 
     struct Chain {
@@ -23,14 +19,37 @@ contract Bridge is Ownable {
         bool active;
     }
 
+    struct RequestInfo {
+        uint256 id;
+        address requestBy;
+        uint256 fromChainId;
+        uint256 fromValue;
+        uint256 toChainId;
+        uint256 toValue;
+        RequestStatus status;
+        address statusDecidedBy;
+        uint256 exchangedAt;
+    }
+
     // mapping, array
+    // @TODO List대신 s로 변경하면 더 좋을듯
     mapping (address => bool) public whiteList;
     mapping (uint256 => Chain) public chainList;
-    Chain[] public chainListArray;
+    mapping (uint256 => RequestInfo) public requestList;
+
+    // error
+    error WhiteListUnauthorizedAccount(address _address);
+    error IncorrectChainId(uint256 chainId);
+    error ChainAlreadyExists(uint256 chainId);
+    error ChainAlreadyRemoved(uint256 chainId);
+    error IncorrectRequestId(uint256 requestId);
+    error IncoorectRequestStatus(RequestStatus requestStatus);
 
     // event
-    event WhitelistUpdated(address _address, bool status);
-    event ChainListUpdated(uint256 chainId, bool status);
+    event WhitelistUpdated(address indexed _address, bool status);
+    event ChainListUpdated(uint256 indexed chainId, bool status);
+    event Requested(address indexed requestAddress, RequestInfo request);
+    event SetRequested(uint256 indexed requestId, RequestStatus indexed requestStatus);
     
     // modifier
     modifier onlyWhiteList {
@@ -38,7 +57,9 @@ contract Bridge is Ownable {
         _;
     }
 
-    constructor() Ownable(msg.sender) {}
+    constructor() Ownable(msg.sender) {
+        whiteList[msg.sender] = true;
+    }
 
     function setWhiteList(address _address, bool status) public onlyOwner {
         whiteList[_address] = status;
@@ -46,27 +67,9 @@ contract Bridge is Ownable {
         emit WhitelistUpdated(_address, status);
     }
 
-    // chainList
-    function getChainIdx(uint256 chainId) internal view returns(uint256, bool) {
-        uint256 chainIdx;
-        bool exists;
-
-        for (uint i = 0; i < chainListArray.length; i++) {
-          if (chainListArray[i].id == chainId) {
-            chainIdx = i;
-            exists = true;
-
-            break;
-          }
-        }
-
-        return(chainIdx, exists);
-    }
-
     function addChain(uint256 chainId) public onlyOwner {
         if (chainList[chainId].active) revert ChainAlreadyExists(chainId);
 
-        chainListArray.push(Chain(chainId, true));
         chainList[chainId].id = chainId;
         chainList[chainId].active = true;
 
@@ -74,21 +77,62 @@ contract Bridge is Ownable {
     }
 
     function removeChain(uint256 chainId) public onlyOwner {
-        (uint256 chainIdx, bool exists) = getChainIdx(chainId);
-        if (!exists) revert IncorrectChainId(chainId);
-        else if (exists && !chainListArray[chainIdx].active) revert ChainAlreadyRemoved(chainId);
+        if (chainList[chainId].id == 0) revert IncorrectChainId(chainId);
+        else if (!chainList[chainId].active) revert ChainAlreadyRemoved(chainId);
         
-        chainListArray[chainIdx].active = false;
         chainList[chainId].active = false;
 
         emit ChainListUpdated(chainId, false);
     }
 
-    // bridge
-    function request() public onlyOwner {
+    // 교환 요청
+    function request(
+        uint256 fromChainId,
+        uint256 toChainId,
+        uint256 value
+    ) public onlyWhiteList {
+        RequestInfo memory req;
+        uint256 requestId = ++requestIdCount;
 
+        req.id = requestId;
+        req.requestBy = msg.sender;
+        req.fromChainId = fromChainId;
+        req.toChainId = toChainId;
+        req.fromValue = value;
+        req.status = RequestStatus.pending;
+
+        // @TODO chainLink 사용해서 가격 받아오기
+        // 교환 비율 지정 (현재 1 : 1)
+        req.toValue = value;
+
+        requestList[requestId] = req;
+        
+        emit Requested(msg.sender, req);
     }
 
-    function setRequest(uint256 requestId, RequestStatus requestStatus) public onlyOwner {
+    // 교환 요청 취소
+    function cancelRequest(uint256 requestId) public onlyWhiteList {
+        RequestInfo storage req = requestList[requestId];
+        if (req.id == 0) revert IncorrectRequestId(requestId);
+        require(req.requestBy == msg.sender, "only the requester can cancel");
+        require(req.status == RequestStatus.pending, "only when status pending can cancel");
+
+        req.status = RequestStatus.canceled;
+        
+        emit SetRequested(requestId, RequestStatus.canceled);
+    }
+
+    // 교환 요청 상태 결정
+    function setRequest(uint256 requestId, RequestStatus status) public onlyOwner {
+        RequestInfo storage req = requestList[requestId];
+
+        if (req.id == 0) revert IncorrectRequestId(requestId);
+        require(req.status == RequestStatus.pending, "only when status pending can setRequest");
+
+        if (status == RequestStatus.approved) req.exchangedAt = block.timestamp;
+        req.status = status;
+        req.statusDecidedBy = msg.sender;
+
+        emit SetRequested(requestId, status);
     }
 }
--- test/mainTest.ts
+++ test/mainTest.ts
@@ -3,10 +3,17 @@ import { Signer } from "ethers";
 import { expect } from "chai"
 import { Bridge } from "../typechain-types";
 
-describe("Bridge Contract Main Test", function () {
+describe("Bridge Contract Main Test", () => {
     let owner: Signer,
         otherAccounts: Signer[],
         bridge: Bridge
+
+    const RequestStatus = {
+        pending: 0n,
+        approved: 1n,
+        rejected: 2n,
+        canceled: 3n
+    };
 
     before(async () => {
         [owner, ...otherAccounts] = await ethers.getSigners();
@@ -16,21 +23,31 @@ describe("Bridge Contract Main Test", function () {
     });
 
     // onlyOwner
-    it ("Auth Test", async () => {
+    it("Auth Test", async () => {
         await expect(bridge
             .connect(otherAccounts[0])
             .setWhiteList(await otherAccounts[0].getAddress(), true)
         ).to.be.revertedWithCustomError(bridge, "OwnableUnauthorizedAccount");
     });
 
-    it("chain add", async () => {
+    // onlyWhiteList
+    it("whiteList Test", async () => {
+        const tx = await bridge
+            .connect(owner)
+            .setWhiteList(otherAccounts[0], true);
+        await tx.wait();
+
+        await expect(tx).to
+            .emit(bridge, "WhitelistUpdated")
+            .withArgs(otherAccounts[0], true);
+    });
+
+    // 체인 추가
+    it("should add chain", async () => {
         const tx = await bridge
             .connect(owner)
             .addChain(1n);
         await tx.wait();
-
-        expect((await bridge.chainListArray(0)).id == 1n);
-        expect((await bridge.chainListArray(0)).active == true);
 
         expect((await bridge.chainList(1n)).id == 1n);
         expect((await bridge.chainList(1n)).active == true);
@@ -44,14 +61,12 @@ describe("Bridge Contract Main Test", function () {
         await expect(tx).to.emit(bridge, "ChainListUpdated");
     });
 
-    it("chain remove", async () => {
+    // 체인 삭제 (비활성화)
+    it("should remove chain", async () => {
         const tx = await bridge
             .connect(owner)
             .removeChain(1n);
         await tx.wait();
-
-        expect((await bridge.chainListArray(0)).id == 1n);
-        expect((await bridge.chainListArray(0)).active == false);
 
         expect((await bridge.chainList(1n)).id == 1n);
         expect((await bridge.chainList(1n)).active == false);
@@ -62,6 +77,113 @@ describe("Bridge Contract Main Test", function () {
             .removeChain(1n)
         ).to.be.revertedWithCustomError(bridge, "ChainAlreadyRemoved");
 
+        // 잘못된 체인 삭제 요청
+        await expect(bridge
+            .connect(owner)
+            .removeChain(2n)
+        ).to.be.revertedWithCustomError(bridge, "IncorrectChainId");
+
         await expect(tx).to.emit(bridge, "ChainListUpdated");
+    });
+
+    // 재활성화 되는지 테스트
+    it("should activate an inactive chain", async () => {
+        const tx = await bridge
+            .connect(owner)
+            .addChain(1n);
+        await tx.wait();
+
+        expect(((await bridge.chainList(1n)).active));
+    });
+
+    // 교환 요청 제출
+    it("should request", async () => {
+        for (let i = 1; i <= 3; i++) {
+            const tx = await bridge
+                .connect(otherAccounts[0])
+                .request(1n, 2n, 1000n);
+            await tx.wait();
+
+            await expect(tx).to.emit(bridge, "Requested");
+
+            const request = await bridge.requestList(i);
+            expect(request.id).to.equal(i);
+            expect(request.requestBy).to.equal(otherAccounts[0]);
+            expect(request.fromChainId).to.equal(1n);
+            expect(request.fromValue).to.equal(1000n);
+            expect(request.toChainId).to.equal(2n);
+            expect(request.toValue).to.equal(1000n);
+            expect(request.status).to.equal(RequestStatus.pending);
+            expect(request.statusDecidedBy).to.equal(ethers.ZeroAddress);
+            expect(request.exchangedAt).to.equal(0n);
+        }
+    });
+
+    // 교환 요청 취소
+    it("should request cancel", async () => {
+        // 잘못된 requestId
+        await expect(bridge
+            .connect(otherAccounts[0])
+            .cancelRequest(99n)
+        ).to.be.revertedWithCustomError(bridge, "IncorrectRequestId");
+
+        // 요청자가 아닌 다른 사람의 취소요청
+        await expect(bridge
+            .connect(owner)
+            .cancelRequest(1n)
+        ).to.be.revertedWith("only the requester can cancel");
+
+        // 취소요청
+        const tx = await bridge
+            .connect(otherAccounts[0])
+            .cancelRequest(1n);
+        await tx.wait();
+
+        expect(tx).to
+            .emit(bridge, "SetRequested")
+            .withArgs(1, RequestStatus.canceled);
+
+        // 이미 처리된 요청에 취소요청
+        await expect(bridge
+            .connect(otherAccounts[0])
+            .cancelRequest(1n)
+        ).to.be.revertedWith("only when status pending can cancel");
+    });
+
+    // 교환 요청 처리
+    it("should request decide", async () => {
+        // 잘못된 requestId
+        await expect(bridge
+            .connect(owner)
+            .setRequest(99n, RequestStatus.approved)
+        ).to.be.revertedWithCustomError(bridge, "IncorrectRequestId");
+
+        // 취소된 요청 처리 시도 할때
+        await expect(bridge
+            .connect(owner)
+            .setRequest(1n, RequestStatus.approved)
+        ).to.be.revertedWith("only when status pending can setRequest");
+
+        // 승인 됐을때
+        const tx2 = await bridge
+            .connect(owner)
+            .setRequest(2n, RequestStatus.approved);
+        await tx2.wait();
+
+        expect(tx2).to.emit(bridge, "SetRequested");
+        expect((await bridge.requestList(2n)).exchangedAt).to.not.equal(0n);
+        expect((await bridge.requestList(2n)).status).to.equal(RequestStatus.approved);
+        expect((await bridge.requestList(2n)).statusDecidedBy).to.equal(await owner.getAddress());
+        
+        // 거절 됐을때
+        const tx3 = await bridge
+            .connect(owner)
+            .setRequest(3n, RequestStatus.rejected);
+        await tx3.wait();
+
+        expect(tx3).to.emit(bridge, "SetRequested");
+        expect((await bridge.requestList(3n)).exchangedAt).to.equal(0n);
+        expect((await bridge.requestList(3n)).status).to.equal(RequestStatus.rejected);
+        expect((await bridge.requestList(3n)).statusDecidedBy).to.equal(await owner.getAddress());
     });
 });
